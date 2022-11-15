568b08cec279d26319f252ba0f048ac31203d582
Eduardo <eduardo.birgiman@outlook.com>
2022-11-15 16:48:33 -0300

    Establishing connection with MongoDB and configuring editorconfig and eslintconfig

---
+++ api/src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+
+
+mongoose.connect('mongodb://localhost:27017')
+	.then(() => {
+		const app = express();
+		const port = 3001;
+
+		app.listen(port, () => {
+			console.log(`🚀 Server is running on http://localhost:${port}`);
+		});
+	})
+	.catch(() => console.log('Erro ao conectar no MongoDB'));
+
