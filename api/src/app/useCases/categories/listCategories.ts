4d75f9f4cffe60979f6da449301b84bb462e8d5f
Eduardo <eduardo.birgiman@outlook.com>
2022-11-17 14:25:54 -0300

    Making Products

--- api/src/app/useCases/categories/listCategories.ts
+++ api/src/app/useCases/categories/listCategories.ts
@@ -3,7 +3,12 @@ import { Request, Response } from 'express';
 import { Category } from '../../models/Category';
 
 export async function listCategories(req: Request, res: Response) {
-	const categories = await Category.find();
+	try {
+		const categories = await Category.find();
 
-	res.json(categories);
+		res.json(categories);
+	} catch(error) {
+		console.log(error);
+		res.sendStatus(500);
+	}
 }
