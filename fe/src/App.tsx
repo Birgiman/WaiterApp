963b468d6658eb2b33557992a06fcc5997444713
Eduardo <eduardo.birgiman@outlook.com>
2022-11-18 16:07:22 -0300

    Making HEADER

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -1,11 +1,14 @@
 import { GlobalStyles } from './styles/GlobalStyles';
+import { Header } from './components/Header/index';
+
+
 
 //React fragment "<>" ;
 export function App() {
 	return (
 		<>
 			<GlobalStyles />
-			<h1>Hello world</h1>
+			<Header />
 		</>
 	);
 }
