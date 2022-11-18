496c4b622532d15791c49bb694c40e7e1fe21a36
Eduardo <eduardo.birgiman@outlook.com>
2022-11-18 14:38:53 -0300

    Making BUILD

---
+++ api/dist/app/models/Category.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Category = void 0;
+const mongoose_1 = require("mongoose");
+exports.Category = (0, mongoose_1.model)('Category', new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    icon: {
+        type: String,
+        required: true,
+    },
+}));
