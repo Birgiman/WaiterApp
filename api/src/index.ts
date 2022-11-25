88bb56d877e78a2cd34618be92fa5d9ab6a8d17c
Eduardo <eduardo.birgiman@outlook.com>
2022-11-25 18:05:59 -0300

    Doind changes on API for work with Front-End

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,17 +1,26 @@
 import path from 'node:path';
+import http from 'node:http';
 import express from 'express';
 import mongoose from 'mongoose';
+import { Server } from 'socket.io';
 
 import { router } from './router';
 
+
+
+const app = express();
+const server = http.createServer(app);
+
+export const io = new Server(server);
+
 mongoose.connect('mongodb://localhost:27017')
 	.then(() => {
-		const app = express();
+
 		const port = 3001;
 
 		app.use((req, res, next) => {
 			res.setHeader('Access-Control-Allow-Origin', '*');
-			res.setHeader('Access-Control-Allow-Methods', '*'); //WILDCARD
+			res.setHeader('Access-Control-Allow-Methods', '*'); // * = WILDCARD
 			res.setHeader('Access-Control-Allow-Headers', '*');
 
 			next();
@@ -20,7 +29,7 @@ mongoose.connect('mongodb://localhost:27017')
 		app.use(express.json());
 		app.use(router);
 
-		app.listen(port, () => {
+		server.listen(port, () => {
 			console.log(`🚀 Server is running on http://localhost:${port}`);
 		});
 	})
